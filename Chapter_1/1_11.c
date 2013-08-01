276e9f935d7a3e3ebd1276ba0b7d5c61a53b736a
Ronni Skansing <Rskansing@gmail.com>
2013-08-02 01:06:18 +0200

    Added the missing 1_11.c

---
+++ Chapter_1/1_11.c
@@ -0,0 +1,5 @@
+
+// How would you test the word count program? What kinds of input 
+// are most likely to uncover bugs if there are any?
+
+// Write a test. But mainly look for overflows, underflows, one-off errors etc.
